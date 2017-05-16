ad9a651032b004ecd5044b3a10003b2b88ea1871
Jonathan Berall <jberall@gmail.com>
2017-05-16 15:09:28 -0700

    Updated the function to include checking if the postal code has been changed.

--- LatLngBehavior.php
+++ LatLngBehavior.php
@@ -37,15 +37,25 @@ class LatLngBehavior extends \yii\base\Behavior {
     {        
             return [
                 ActiveRecord::EVENT_BEFORE_INSERT => 'assignLatLng',
-                ActiveRecord::EVENT_BEFORE_UPDATE => 'assignLatLng',
-                       
+                ActiveRecord::EVENT_BEFORE_UPDATE => 'assignLatLng',      
             ];
     }
-
+/**
+ * This function calls GetLatLng::getLatLngGoogle($this->owner->toArray(),$this->google_api_key);
+ * Only when their has been a change to the postal_code.
+ * 
+ * @throws InvalidParamException if no latlng attribute exists.
+ */
     public function assignLatLng() {
         if (!$this->owner->hasProperty($this->latlng)) {
             throw new InvalidParamException(get_class($this->owner) . ' has no attribute property named "' . $this->latlng . '".');
         }
+        //don't preceed if same postal code
+        if (!$this->owner->isAttributeChanged('postal_code',false)) {
+//            die(__METHOD__);
+            return ;
+        }
+//        die(__METHOD__.$this->google_api_key);
         $arrLatLng = GetLatLng::getLatLngGoogle($this->owner->toArray(),$this->google_api_key);
         $this->owner{$this->latlng} = $arrLatLng['latlng'] ?? null;
     }
